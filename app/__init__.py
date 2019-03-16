cdf7ba7d26f96f5795ea423c8d9f5a4ed8aa0dc2
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2019-03-16 12:18:54 +0700

    #5 Add migration and models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
 
 # tell flask to read it and apply it
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
